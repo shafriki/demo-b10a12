de500669a7e734b8c8413ff088ec5d4e5658e2c0
shafriki <shafriki.cse@gmail.com>
2025-01-12 08:23:05 +0600

    added some pages and other necesary forders

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,9 @@
-import React from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay';
-
-// import required modules
-import { FreeMode, Pagination, Autoplay } from 'swiper/modules';
-
-export default function App() {
+const App = () => {
   return (
-    <>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        freeMode={true}
-        autoplay={{
-          delay: 2500, // Delay between slides in milliseconds
-          disableOnInteraction: false, // Continue autoplay after interaction
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination, Autoplay]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </>
+    <div>
+      
+    </div>
   );
-}
+};
+
+export default App;
